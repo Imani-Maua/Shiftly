0a1fb67dec3bc792abd9e9a3690eab3f5d3be04c
Maua Imani <imanimaua@gmail.com>
2025-10-15 06:56:17 +0200

    Added user role based dependencies

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, APIRouter, Depends, HTTPException, status, Body
 from fastapi.security import OAuth2PasswordRequestForm
-from jose import JWTError
+from jose import jwt, JWTError
+import os
+import asyncio
 import asyncpg
 from typing import Annotated
 from datetime import timedelta, datetime, timezone
-from app.auth.security import authenticate_user, user_in_db, verify_token, create_access_token, token_in_db, get_user, required_roles
-from app.auth.models import Token, UserInDB, TokenPayload, UserInvite, AcceptInvite, InviteToken, sendRequest, createUser
+from app.auth.security import authenticate_user, user_in_db, verify_token_type, create_jwt, token_in_db, get_user, required_roles, store_token
+from app.auth.models import Token, UserInDB, TokenPayload, UserInvite, AcceptInvite, InviteToken, sendRequest, createUser, UserRole
 from app.database.database import get_db, asyncSQLRepo
 from app.auth.utils import send_email, hash_password, generate_temporary_password, verify_password
 
@@ -13,11 +15,15 @@ from app.auth.utils import send_email, hash_password, generate_temporary_passwor
 app = FastAPI()
 router = APIRouter()
 
+SECRET_KEY = os.getenv("KEY")
+algorithm = "HS256"
+
+
 @app.post("/users/create", response_model=UserInvite)
 async def create_user(user: Annotated[createUser, Body()],
                        db: Annotated[asyncpg.Connection, Depends(get_db)],
-                       _: str = Depends(required_roles("super user", "admin"))) -> UserInvite:
-    
+                        _: str = Depends(required_roles(UserRole.superuser, UserRole.admin))) -> UserInvite:
+     
     username = f"{user.firstname.strip().lower()}.{user.lastname.strip().lower()}"
     user_exists = await user_in_db(db, username=username)
     if user_exists:
@@ -27,13 +33,14 @@ async def create_user(user: Annotated[createUser, Body()],
     hashed = hash_password(temporary)
     row = await db.fetchrow(insert_query, username, user.firstname, user.lastname, user.email, user.user_role, hashed, False) 
     id = row["id"]
-    return UserInvite(sub=id,username=username, email=user.email, role=user.user_role, password=temporary) #sub is not an integer?
+    return UserInvite(sub=id,username=username, email=user.email, role=user.user_role, password=temporary) 
+
 
 INVITE_EXPIRY_HOURS = 24
 @app.post("/users/invite", response_model=dict)
 async def invite_user(user_id:Annotated[sendRequest, Body()], 
                       db: Annotated[asyncpg.Connection, Depends(get_db)],
-                      _: str =Depends(required_roles("super user", "admin"))):
+                      _: str = Depends(required_roles(UserRole.superuser, UserRole.admin))):
     user: UserInDB = await get_user(db=db, id=user_id.user_id)
     if not user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User does not exist")
@@ -41,12 +48,11 @@ async def invite_user(user_id:Annotated[sendRequest, Body()],
         payload = TokenPayload(sub=user.username, id=user.id, email=user.email, role=user.user_role, type="invite")
     except:
         raise ValueError("Incorrect data type")
-    access_token_expires = timedelta(hours=INVITE_EXPIRY_HOURS)
-    invite_token: InviteToken = await create_access_token(db=db, data=payload, expiry=access_token_expires, type=payload.type)
-    #this is the first point where the token is created
-    #the token is created, hashed, then stored in the database
+    invite_token_expires = timedelta(hours=INVITE_EXPIRY_HOURS)
+    invite_token: InviteToken = create_jwt(data=payload, expiry=invite_token_expires)
+    stored_invite = await store_token(data=payload, jwt=invite_token, db=db)
+
     invite_link = f"https://shiftly.app/register?token={invite_token}"
-    #and we send the token - unhashed to the user
     name = user.username.split(".")[0].capitalize()
 
     subject = "You've been invited to Shiftly!"
@@ -67,39 +73,35 @@ You’ve been invited to join Shiftly as a {user.user_role}.
     return {"message": f"Invite sent to {user.email}"}
 
 @app.post("/invite/accept", response_model=dict)
-async def accept_invite(data: Annotated[AcceptInvite, Body ()], db: Annotated[asyncpg.Connection, Depends(get_db)]):
+async def accept_invite(data: Annotated[AcceptInvite, Body ()], 
+                        db: Annotated[asyncpg.Connection, Depends(get_db)]):
     try:
-        verify_type= verify_token(data.token, "invite")
+        verify_type= verify_token_type(data.token, "invite")
         payload = TokenPayload(**verify_type)
-       # import pdb; pdb.set_trace()
-        token: InviteToken = await token_in_db(db, data.token)
+        token: InviteToken = await token_in_db(db, data.token) 
         if not token:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Token missing.")
         
-        if datetime.now(timezone.utc) > payload.exp:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or expired token. Ask your admin for a new token")
+        if datetime.now(timezone.utc) > payload.exp: #using timezone.utc does not cause issues check why since when sending the invite, it does
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Expired token. Ask your admin for a new token")
 
         user_id = payload.id
         user:UserInDB = await user_in_db(db, id=user_id)
         if not user:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-        
         hashed_pass = hash_password(data.new_password)
         user_query = f"UPDATE users SET pwd_hash = $1, is_active = {True} WHERE id= $2"
+        payload = jwt.decode(data.token,SECRET_KEY, algorithms=algorithm)
+        jti = payload.get('jti')
         activate_user = await asyncSQLRepo(conn=db, query=user_query, params=(hashed_pass, user_id,)).execute()
-        token_query = f"UPDATE invite_token SET used_at = $1 WHERE token = $2"
-        set_used_time = await asyncSQLRepo(conn=db, query=token_query, params=(datetime.now(), data.token,)).execute()
-        if user:
-            user_token = TokenPayload(sub=user.username, id=user.id, email=user.email, role=user.user_role, type="access")
-        else:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-        access_token = await create_access_token(db=db, data=user_token, type="access")
+        token_query = f"UPDATE invite_token SET used_at = $1 WHERE jti = $2"
+        set_used_time = await asyncSQLRepo(conn=db, query=token_query, params=(datetime.now(), jti,)).execute()
+        return {"message": "Account activated successfully! Click here to login"}
         
-        return {"message": "Password set! You are now logged in!",
-                "access_token": access_token}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired invite token")
 
+ACCESS_EXPIRY_DAYS = 4
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                   db: Annotated[asyncpg.Connection, Depends(get_db)]) -> Token:
@@ -122,13 +124,14 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
         role = user.user_role,
         type = "access"
     )
-    access_token = await create_access_token(db=db, data=payload, type=payload.type)
+    access_token_expires = timedelta(days=ACCESS_EXPIRY_DAYS)
+    access_token = create_jwt(data=payload, expiry=access_token_expires)
     return Token(access_token=access_token, token_type="bearer", role=user.user_role)
        
 @app.get("/invite/accept", response_model=dict)
 async def get_invite(token:str, db: Annotated[asyncpg.Connection, Depends(get_db)]):
     try:
-        payload = verify_token(token, "invite")
+        payload = verify_token_type(token, "invite")
         user_id = payload.get("id")
         token = await token_in_db(db, token)
         if not token:
@@ -139,3 +142,5 @@ async def get_invite(token:str, db: Annotated[asyncpg.Connection, Depends(get_db
         return {"email": user.email}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired invite token")
+
+
