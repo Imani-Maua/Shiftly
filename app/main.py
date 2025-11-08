adcaa27a5c51b73a00b127491183a1410e0fcefc
Maua Imani <imanimaua@gmail.com>
2025-11-08 07:16:10 +0100

    Added new API router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app.auth.routes import auth_router
-from app.core.services.routes import schedule
+from app.core.services.routes import schedule, shift_period
 
 app = FastAPI(title="Shiftly", version="1.0")
 app.include_router(auth_router, prefix="/users")
 app.include_router(schedule, prefix="/schedule")
+app.include_router(shift_period, prefix="/shift_period")
+
 
 
 
