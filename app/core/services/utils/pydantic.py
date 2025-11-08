8d3fc5fe0cfed9991a353ea2aff4759fba28a699
Maua Imani <imanimaua@gmail.com>
2025-11-08 05:23:02 +0100

    Refactored pydantic models for shift periods and templates

--- app/core/services/utils/pydantic.py
+++ app/core/services/utils/pydantic.py
@@ -19,27 +19,23 @@ class Staffing(str, Enum):
     low = "low"
     high = "high"
 
-class ShiftCreate(BaseModel):
-    staffing: Staffing
-    shift_name: ShiftName
-    start_time: time
-    end_time: time
+class ShiftTemplateCreate(BaseModel):
     role: str
     role_count: int
 
-class ShiftUpdate(BaseModel):
-    staffing: Staffing | None = None
+class ShiftPeriodCreate(BaseModel):
+    shift_name: ShiftName
+    start_time: time
+    end_time: time
+
+class ShiftPeriodUpdate(BaseModel):
     shift_name: ShiftName  | None = None
     start_time: time  | None = None
     end_time: time  | None = None
-    role: str  | None = None
-    role_count: int  | None = None
-
+   
 class ShiftOut(BaseModel):
     id: int
     shift_name: str
-    role_name: str
     start_time: time
     end_time: time
-    staffing: str
 
