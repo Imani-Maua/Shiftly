80aa6cb4f6958f8ae239e284ee35837400f6fc14
Maua Imani <imanimaua@gmail.com>
2025-11-10 22:18:02 +0100

    added exceptions class handler

--- app/main.py
+++ app/main.py
@@ -1,11 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from app.core.utils.exceptions import AppBaseException
+from app.core.routes.schedule_route import schedule
+from app.core.routes.talent_route import talents
 from app.auth.routes import auth_router
-from app.core.services.routes import schedule, shift_period
+
 
 app = FastAPI(title="Shiftly", version="1.0")
+
+
+@app.exception_handler(AppBaseException)
+async def app_exception_handler(request: Request, exc: AppBaseException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": exc.detail, "path": str(request.url)}
+    )
+
+@app.exception_handler(Exception)
+async def generic_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"error": "Unexpected internal server error"}
+    )
+
 app.include_router(auth_router, prefix="/users")
 app.include_router(schedule, prefix="/schedule")
-app.include_router(shift_period, prefix="/shift_period")
+app.include_router(talents, prefix="/talents")
+
+
+
 
 
 
