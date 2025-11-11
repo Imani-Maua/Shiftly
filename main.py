0e3c0b1b20e4af60d6f40b0e83b41025eb10e164
Maua Imani <imanimaua@gmail.com>
2025-11-11 22:15:48 +0100

    Refactored global error handling to error handling in the controllers

--- main.py
+++ main.py
@@ -4,29 +4,15 @@ from app.core.utils.exceptions import AppBaseException
 from app.core.routes.schedule_route import schedule
 from app.core.routes.talent_route import talents
 from app.auth.routes.routes import auth_router
+from app.core.routes.talent_constraint_route import talent_constraints
 
 
 app = FastAPI(title="Shiftly", version="1.0")
 
-
-
-@app.exception_handler(AppBaseException)
-async def app_exception_handler(request: Request, exc: AppBaseException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"error": exc.detail, "path": str(request.url)}
-    )
-
-@app.exception_handler(Exception)
-async def generic_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content={"error": "Unexpected internal server error"}
-    )
-
 app.include_router(auth_router, prefix="/users")
 app.include_router(schedule, prefix="/schedule")
 app.include_router(talents, prefix="/talents")
+app.include_router(talent_constraints, prefix="/talent_constraints")
 
 
 
