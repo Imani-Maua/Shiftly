9c4de063b4b152a0e78377a6e2147092c0e4a89d
Maua Imani <imanimaua@gmail.com>
2025-11-19 02:46:44 +0100

    Added API endpoint for creating shift template

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from app.auth.routes import auth_router
 from app.core.constraints.talent_constraints.routes import talent_constraints
 from app.core.constraints.constraint_rules.routes import constraint_rules
 from app.core.shift_period.routes import shift_period
+from app.core.shift_template.routes import shift_templates
 
 
 app = FastAPI(title="Shiftly", version="1.0")
@@ -17,6 +18,7 @@ app.include_router(talents, prefix="/talents")
 app.include_router(talent_constraints, prefix="/talent_constraints")
 app.include_router(constraint_rules, prefix="/constraint_rules")
 app.include_router(shift_period, prefix="/shift_period")
+app.include_router(shift_templates, prefix="/shift_templates")
 
 
 
